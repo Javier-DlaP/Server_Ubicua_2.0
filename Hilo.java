86e3ae2a5f2f75a3835e0a44797b048dae47af77
PkVc09 <pokevocaloid09@gmail.com>
2020-04-25 13:07:40 +0200

    Inicializados el servidor y las conexiones por hilo

--- Hilo.java
+++ Hilo.java
@@ -1,4 +1,39 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
-public class Hilo {
+public class Hilo implements Runnable {
 
+    private Socket socket;
+
+    public Hilo(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter salida = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            switch (entrada.readLine()) {
+                case "arduino":
+                    Mensaje mensaje = new Mensaje();
+                    while (true) {
+
+                    }
+                case "usuario":
+
+                    break;
+                default:
+                    break;
+            }
+            entrada.close();
+            socket.close();
+        } catch (Exception e) {}
+        finally {
+            try {
+                socket.close();
+            } catch (Exception e) {}
+        }
+    }
 }
--- Servidor.java
+++ Servidor.java
@@ -1,4 +1,27 @@
+import java.util.concurrent.*;
+import java.net.*;
 
 public class Servidor {
 
+    public static void main(String[] args) {
+        try {
+            int puerto = 0;
+            ServerSocket serverSocket = new ServerSocket(puerto);
+            ExecutorService conexiones = Executors.newCachedThreadPool();
+
+            System.out.println("Servidor encendido.");
+
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    Hilo conexion = new Hilo(socket);
+                    conexiones.execute(conexion);
+                } catch (Exception e) {
+                    System.out.println("No se ha podido conectar con el cliente o se ha desconectado.");
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("Conexión inválida.");
+        }
+    }
 }
