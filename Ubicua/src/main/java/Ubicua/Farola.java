3ff26ccef14f00814bd337ec2ac55f6b157ddadf
PkVc09 <pokevocaloid09@gmail.com>
2020-08-11 21:32:08 +0200

    Bucle socket

--- Ubicua/src/main/java/Ubicua/Farola.java
+++ Ubicua/src/main/java/Ubicua/Farola.java
@@ -4,16 +4,16 @@ import java.util.*;
 
 public class Farola {
     private int posArrayHoraActual = -1;
-    private ArrayList<Float> aux_luces[];
-    private ArrayList<Float> aux_sensores[];
+    private ArrayList<Integer> aux_luces;
+    private ArrayList<Integer> aux_sensores;
     private float luces[];
     private float sensores[];
     private int id;
     private int ldr;
 
     public Farola(int id){
-        aux_luces = new ArrayList[48];
-        aux_sensores = new ArrayList[48];
+        aux_luces = new ArrayList<>();
+        aux_sensores = new ArrayList<>();
         luces = new float[48];
         sensores = new float[48];
         Arrays.fill(luces, 0);
@@ -21,19 +21,27 @@ public class Farola {
         this.id = id;
     }
 
-    public void anadir(int posArrayHora, float movement){
-        if(posArrayHoraActual==-1){
-            posArrayHoraActual=posArrayHora;
-        }else{
-            if(posArrayHoraActual!=posArrayHora){
-                //Crear hilo para hacer medias
-            }
-        }
-        //aux_luces[posArrayHora].add(light);
-        aux_sensores[posArrayHora].add(movement);
+    public void anadir(int posArrayHora, int movement, int ldr, boolean hacerMedia){
+        if (hacerMedia) calcularMedias(posArrayHora);
+        
+        aux_luces.add(ldr);
+        aux_sensores.add(movement);
     }
 
     public void cambiarLuz(int posArrayHora, float light) {
         luces[posArrayHora] = light;
+    }
+
+    public void calcularMedias(int pos) {
+        float sumaAuxLuces = 0;
+        float sumaAuxSensores = 0;
+
+        for (int i = 0; i < aux_luces.size(); i++) {
+            sumaAuxLuces += aux_luces.get(i);
+            sumaAuxSensores += aux_sensores.get(i);
+        }
+
+        luces[pos] = sumaAuxLuces / aux_luces.size();
+        sensores[pos] = sumaAuxSensores / aux_sensores.size();
     }
 }
